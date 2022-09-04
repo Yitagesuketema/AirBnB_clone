219fcaf0b39b742035ecd26ad8b207434d23d6bf
Yitagesu Ketema <yitagesuketema@gmail.com>
2022-09-04 09:02:12 +0300

    final correction

--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
-"""Defining BaseModel class."""
+"""Defines the BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """the BaseModel of the HBnB project."""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialization of a new BaseModel.
+        """Initialize a new BaseModel.
 
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
